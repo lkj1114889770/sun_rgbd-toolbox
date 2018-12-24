d5bde1a4258c01f54daebdc7b9ca49d6a282f60f
lkj <21710207@zju.edu.cn>
2018-12-24 23:10:31 +0800

    change the way to get bbox by meta2DBB

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-A simple python based toolbox to get bbox2D for the SUN_RGBD dataset.
+A simple python based toolbox to get bbox2D for the SUN_RGBD dataset with the [SUNRGBDMeta2DBB_v2.mat](http://rgbd.cs.princeton.edu/data/SUNRGBDMeta2DBB_v2.mat)
 The sun-rgbd dataset can be downloaded from [here](http://rgbd.cs.princeton.edu/data/SUNRGBD.zip), please see the [offical webpage](http://rgbd.cs.princeton.edu/) for more details about the data.
 
 You can test the bbox annotation like this after dumps the lables_bbox2D.json file.
-
+	
 	def apply_bbox(image,bbox):
 	    draw=ImageDraw.Draw(image)
 	    x1,y1,x2,y2=bbox
 	    draw.line((x1,y1,x2,y1,x2,y2,x1,y2,x1,y1),'red')
 	    return image
+	with open('./lables_bbox2D.json') as open_file:
+    	annotations = json.load(open_file)
 	img=Image.open(annotations[10]['image'])
-	print(annotations[10]['annotation'][4]['class_name'])
-	apply_bbox(img,bbox=annotations[10]['annotation'][4]['bbox']) 
+	print(annotations['100']['annotation']['0']['class_name'])
+	apply_bbox(img,bbox=annotations['100']['annotation']['0']['bbox']) 
 
 ![](/img/1.png)
--- get_bbox2D.py
+++ get_bbox2D.py
@@ -1,72 +1,30 @@
 import os
 import json
 import numpy as np
-import cv2
+import scipy.io
 
-root_dir = '/home/zju/lkj/data/SUNRGBD/'  # root directory of the sun-rgbd dataset, it should be changed to your directory
-all_paths= os.walk(root_dir)
-all_paths=list(all_paths)
-imgs_path=[]
+mat = scipy.io.loadmat('./SUNRGBDMeta2DBB_v2.mat')
+meta2Dbb = mat['SUNRGBDMeta2DBB'][0]
 
-# Traverses all subpaths to find all image path
-for maindir, subdir, file_name_list in all_paths:
-    if 'seg.mat' in file_name_list:
-        image_path = os.path.join(maindir,'image/')
-        assert len(os.listdir(image_path))==1 # aseert only 1 image in dir 'image/'
-        image_path = os.listdir(image_path)[0] 
-        img_path = os.path.join(maindir,'image/'+image_path) 
-        imgs_path.append(img_path)
-        
-annotations_path=[]
-for img_path in imgs_path:
-    annotation_file_path  = '/'.join(img_path.split('/')[:-2])+'/annotation/index.json'
-    assert os.path.exists(annotation_file_path)
-    annotations_path.append(annotation_file_path)
-
-def get_annotations(imgs_path,annotations_path):
-    print('start to index...')
-    bbox2Dfiles = {}
-    idx=0
-    for img_path,annotation_file_path in zip(imgs_path,annotations_path):
-        img = cv2.imread(img_path)
-        H,W,C=img.shape
-        depth = os.listdir('/'.join(img_path.split('/')[:-2])+'/depth/')[0]
-        depth_path = os.path.join('/'.join(img_path.split('/')[:-2])+'/depth/',depth)
-        assert os.path.exists(depth_path)
-        bbox2Dfiles[idx]={"image":img_path,"depth":depth_path,"annotation":{}}
-		
-        try:
-            with open(annotation_file_path) as open_file:
-                data = json.load(open_file)
-        except:  # some of the json file can not be loaded, just skip
+bbox2D={}
+idx=0
+for i in range(len(meta2Dbb)):
+    if meta2Dbb[i][1].shape[1]<=0: # ensure contain 2D bbox
+        continue
+    img_path = '/home/zju/lkj/data/'+'/'.join(meta2Dbb[i][3][0].split('/')[5:])  # the image and depth path should be changed according to your path
+    img = cv2.imread(img_path)
+    ih,iw,ic=img.shape
+    depth_path = '/home/zju/lkj/data/'+'/'.join(meta2Dbb[i][2][0].split('/')[5:])
+    bbox2D[idx]={'image':img_path,'depth':depth_path,'annotation':{}}
+    ibox=0
+    for ele in meta2Dbb[i][1][0]:
+        name = ele[2][0]
+        x,y,w,h=ele[1][0].tolist()
+        bbox=[max(int(x),0),max(int(y),0),min(int(x+w),iw),min(int(y+h),ih)]
+        if bbox[0]>=bbox[2] or bbox[1]>=bbox[3]:
             continue
-        if len(data['objects'])<=max(list(map(lambda x: x['object'],data["frames"][0]["polygon"]))):
-            continue
-        numberOfAnot = len(data["frames"][0]["polygon"])
-        objs={}
-        for i in range(0,numberOfAnot):
-            x = data["frames"][0]["polygon"][i]["x"]
-            y = data["frames"][0]["polygon"][i]["y"]
-            if x==[] or not isinstance(x,list):  # skip when x==[] or x is not a list without more than 2 elements
-                continue
-            idxObj = data["frames"][0]["polygon"][i]["object"]
-            pts2 = np.array([x,y], np.int32)
-            pts2 = np.transpose(pts2)
-            
-            x1,x2=int(np.min(pts2[:,0])),int(np.max(pts2[:,0]))
-            y1,y2=int(np.min(pts2[:,1])),int(np.max(pts2[:,1]))
-            
-            if idxObj not in objs.keys():
-                objs[idxObj]={'class_name':data['objects'][idxObj]["name"],'bbox':[max(x1,0),max(y1,0),min(x2,W),min(y2,H)]}
-            else:
-                x11,y11,x22,y22=objs[idxObj]['bbox']
-                objs[idxObj]['bbox']=[max(min(x11,x1),0),max(min(y11,y1),0),min(max(x22,x2),W),min(max(y22,y2),H)]
-        bbox2Dfiles[idx]['annotation']=objs
-        idx = idx +1
-    print('There are %d index.json have been indexed!'%(idx))
-    return bbox2Dfiles
-#     print(idx)
-annotations = get_annotations(imgs_path,annotations_path)  
-
+        bbox2D[idx]['annotation'][ibox]={'bbox':bbox,'class_name':name.lower()}
+        ibox = ibox+1
+    idx=idx+1
 with open('./lables_bbox2D.json', 'w') as fileObject:
-        json.dump(annotations,fileObject)    
+        json.dump(bbox2D,fileObject) 
